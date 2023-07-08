faa2d054261d315e0108c3cccabfc3b80ccb9018
papito1111 <patrykrychly@gmail.com>
2023-07-08 14:23:02 +0200

    eksperymenty

--- Src/ExampleApi/Controllers/WeatherForecastController.cs
+++ Src/ExampleApi/Controllers/WeatherForecastController.cs
@@ -14,14 +14,19 @@ public class WeatherForecastController : ControllerBase
 
     private readonly ILogger<WeatherForecastController> _logger;
 
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    private readonly Burza Lejewkit;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger, Burza lejewkit)
     {
         _logger = logger;
+        Lejewkit = lejewkit;
     }
 
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
+         int i = 5;
+        Lejewkit.CzyPada();
         return Enumerable.Range(1, 5).Select(index => new WeatherForecast
         {
             Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
@@ -34,11 +39,12 @@ public class WeatherForecastController : ControllerBase
     [HttpGet("{date}", Name = "GetWeatherForecastForAnotherDay")]
     public WeatherForecast Get(DateTime date)
     {
-        return new WeatherForecast
-        {
-            Date = new DateOnly(date.Year,date.Month, date.Day),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        };
+        throw new Exception("Ups");
+        //return new WeatherForecast
+        //{
+        //    Date = new DateOnly(date.Year,date.Month, date.Day),
+        //    TemperatureC = Random.Shared.Next(-20, 55),
+        //    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        //};
     }
 }
--- Src/ExampleApi/Program.cs
+++ Src/ExampleApi/Program.cs
@@ -1,8 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
+// Add services to the container.
+var tmp = builder.Configuration.GetValue<string>("ConnectionString");
 builder.Services.AddControllers();
+builder.Services.AddScoped<Burza>(x => new Burza(tmp));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -18,8 +20,107 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
 
+app.UseMiddleware<ExeptionMiddleWare>();
 app.MapControllers();
+app.UseMiddleware<TimeMiddleWare>();
+
+app.Map("tmp", () => new { x = "pyszny" });
+
+new tmp(app.Services);
 
 app.Run();
+
+
+class ExeptionMiddleWare
+{
+
+    private readonly RequestDelegate _next;
+    private readonly ILogger<ExeptionMiddleWare> logger;
+
+    public ExeptionMiddleWare(RequestDelegate next, ILogger<ExeptionMiddleWare> logger)
+    {
+        _next = next;
+        this.logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (ValidationException)
+        {
+            context.Response.StatusCode = 400;
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "B³ad w requescie");
+            context.Response.StatusCode = 500;
+            context.Response.WriteAsJsonAsync(ex);
+        }
+    }
+}
+
+class TimeMiddleWare
+{
+
+    private readonly RequestDelegate _next;
+    private readonly ILogger<ExeptionMiddleWare> logger;
+
+    public TimeMiddleWare(RequestDelegate next, ILogger<ExeptionMiddleWare> logger)
+    {
+        _next = next;
+        this.logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        var time = System.Diagnostics.Stopwatch.GetTimestamp();
+        try
+        {
+            await _next(context);
+        }
+        finally
+        {
+            var elapsed = System.Diagnostics.Stopwatch.GetElapsedTime(time);
+            logger.LogInformation("Method executed in {Time}", elapsed);
+        }
+    }
+}
+public class ValidationException : Exception
+{
+
+}
+
+public class Burza
+{
+    private static int number;
+    private readonly string connectionString;
+
+    public Burza(string connectionString)
+    {
+        number = number + 1;
+        this.connectionString = connectionString;
+    }
+    public void CzyPada()
+    {
+
+    }
+}
+
+public class tmp
+{
+    public tmp(IServiceProvider serviceDescriptors)
+    {
+        using (var scope = serviceDescriptors.CreateScope())
+        {
+            var burza = scope.ServiceProvider.GetRequiredService<Burza>();
+            burza = scope.ServiceProvider.GetRequiredService<Burza>();
+            burza = scope.ServiceProvider.GetRequiredService<Burza>();
+            burza = scope.ServiceProvider.GetRequiredService<Burza>();
+        }
+
+    }
+}
